6291b7fc7d9727cc8508d8c48bbdcde9be41611c
extensaman <verus.wedding@gmail.com>
2021-12-04 18:52:14 +0300

    Раздел "Generics и Enum"  * Задание №35  *  * Создать eпum, который описывает сезоны года. Добавить поле description в  * этот enum. Добавить поле couпtOfDays в этот enum. Вывести на экран все константы  * сезоны года.

---
+++ src/home/chapter07genericandenum/task35/Seasons.java
@@ -0,0 +1,25 @@
+package home.chapter07genericandenum.task35;
+
+public enum Seasons {
+
+    WINTER (90, "snowy"),
+    SPRING (92, "warm"),
+    SUMMER (92, "hot"),
+    AUTUMN (92, "cold");
+
+    private int countOfDays;
+    private String description;
+
+    Seasons(int countOfDays, String description) {
+        this.countOfDays = countOfDays;
+        this.description = description;
+    }
+
+    public int getCountOfDays() {
+        return countOfDays;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
